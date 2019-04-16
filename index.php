18c9e6314dadb9a7b6413e57ca0b1fa0e18e2845
slehmann <s.lehmann@esemos.de>
2019-04-16 16:19:01 +0200

    EDT: Einfügen von AMCharts

--- index.php
+++ index.php
@@ -16,7 +16,17 @@
 <meta name="robots" content="noindex,nofollow" />
 <link rel="stylesheet" type="text/css" href="style.css" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+
+<style>
+#chartdiv {
+  width: 100%;
+  height: 500px;
+}
+</style>
+
+
 </head>
 <body>
 <div id="maincontent">
@@ -36,6 +46,8 @@ while ($row = $results->fetchArray()) {
 //echo "</div>":
         ?>
         
+        
+        <div id="chartdiv"></div>
            </div>
     
     <div id="rightcontent">
@@ -52,6 +64,82 @@ while ($row = $results->fetchArray()) {
 
     
   </div>
+<!-- Resources -->
+<script src="https://www.amcharts.com/lib/4/core.js"></script>
+<script src="https://www.amcharts.com/lib/4/charts.js"></script>
+<script src="https://www.amcharts.com/lib/4/themes/animated.js"></script>
 
+<!-- Chart code -->
+<script>
+
+// Themes begin
+am4core.useTheme(am4themes_animated);
+// Themes end
+
+// Create chart instance
+var chart = am4core.create("chartdiv", am4charts.XYChart);
+
+// Export
+chart.exporting.menu = new am4core.ExportMenu();
+
+// Data for both series
+var data = [ {
+  "date": "2009-12-12",
+  "luftfeuchtigkeit": 50.0,
+  "temperatur": 21.1
+}, {
+  "date": "2010-12-12",
+  "luftfeuchtigkeit": 51.2,
+  "temperatur": 21.2
+}];
+
+/* Create axes */
+var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+categoryAxis.dataFields.category = "date";
+categoryAxis.renderer.minGridDistance = 30;
+
+/* Create value axis */
+var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+valueAxis.title.text = "Temperatur in °C";
+
+var durationAxis = chart.yAxes.push(new am4charts.DurationAxis());
+durationAxis.title.text = "Luftfeuchtigkeit in %";
+durationAxis.baseUnit = "minute";
+durationAxis.renderer.grid.template.disabled = true;
+durationAxis.renderer.opposite = true;
+
+/* Create series */
+var columnSeries = chart.series.push(new am4charts.ColumnSeries());
+columnSeries.name = "luftfeuchtigkeit";
+columnSeries.dataFields.valueY = "luftfeuchtigkeit";
+columnSeries.dataFields.categoryX = "date";
+
+columnSeries.columns.template.tooltipText = "[#fff font-size: 15px]{name} in {categoryX}:\n[/][#fff font-size: 20px]{valueY}%[/] [#fff]{additional}[/]"
+columnSeries.columns.template.propertyFields.fillOpacity = "fillOpacity";
+columnSeries.columns.template.propertyFields.stroke = "stroke";
+columnSeries.columns.template.propertyFields.strokeWidth = "strokeWidth";
+columnSeries.columns.template.propertyFields.strokeDasharray = "columnDash";
+columnSeries.tooltip.label.textAlign = "middle";
+
+var lineSeries = chart.series.push(new am4charts.LineSeries());
+lineSeries.name = "Temperatur";
+lineSeries.dataFields.valueY = "temperatur";
+lineSeries.dataFields.categoryX = "date";
+
+lineSeries.stroke = am4core.color("#ff0000");
+lineSeries.strokeWidth = 3;
+lineSeries.propertyFields.strokeDasharray = "lineDash";
+lineSeries.tooltip.label.textAlign = "middle";
+
+var bullet = lineSeries.bullets.push(new am4charts.Bullet());
+bullet.fill = am4core.color("#ff0000"); // tooltips grab fill from parent by default
+bullet.tooltipText = "[#fff font-size: 15px]{name} in {categoryX}:\n[/][#fff font-size: 20px]{valueY}°C[/] [#fff]{additional}[/]"
+var circle = bullet.createChild(am4core.Circle);
+circle.radius = 4;
+circle.fill = am4core.color("#fff");
+circle.strokeWidth = 3;
+
+chart.data = data;
+</script>
 </body>
 </html>
