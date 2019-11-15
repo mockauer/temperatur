7bcf9dbdad24cfec9fcea291b0e59f49104da054
S. Lehmann <barrus@gmx.de>
2019-11-15 19:26:47 +0100

    ADD: Menu hinzufügen

---
+++ menu.php
@@ -0,0 +1,3 @@
+  <div class="links">
+   | <a class="site_link" href="index.php" target="_self">Start</a>| <a class="site_link" href="diagramm.php" target="_self">Diagramm</a> | <a class="site_link" href="tabelle.php" target="_self">Tabelle</a> | 
+    </div>
